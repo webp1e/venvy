101c14677437f6e5c1c787ef83a30209c4bd3a6e
webp1e <w3bpie@gmail.com>
2026-05-03 21:00:59 +0400

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -5,7 +5,7 @@ echo "установщик venvy..."
 
 cat > /usr/local/bin/venvy << 'EOF'
 #!/bin/bash
-exec /bin/bash --rcfile <(echo "PS1='(venv) \[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '"; source /home/sigma/venv/bin/activate)
+exec /bin/bash --rcfile <(echo "PS1='(venv) \[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '"; source $HOME/venv/bin/activate)
 EOF
 
 chmod +x /usr/local/bin/venvy
